333face8afd2963b5c31d73aede21cd1b29feab5
Allison Payne <allison@apayneinthe.app>
2023-01-13 20:04:57 -0500

    Fix line endings

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,11 +1,60 @@
 <script setup lang="ts">
-import RouterView from './router/RouterView.vue';
-import AppBar from './components/AppBar.vue';
+import { RouterView } from 'vue-router';
+//import RouterView from './router/RouterView.vue';
+//import AppBar from './components/AppBar.vue';
+</script>
+
+<script lang="ts">
+export default {
+  data: () => ({
+    pageTitle: 'DeTails',
+    drawerVisible: false,
+    group: null,
+    items: [
+      {
+        title: 'Foo',
+        value: 'foo',
+      },
+      {
+        title: 'Bar',
+        value: 'bar',
+      },
+      {
+        title: 'Fizz',
+        value: 'fizz',
+      },
+      {
+        title: 'Buzz',
+        value: 'buzz',
+      },
+    ],
+  }),
+  watch: {
+    group() {
+      this.drawerVisible = false;
+    },
+  },
+};
 </script>
 
 <template>
   <v-app>
-    <AppBar />
-    <RouterView />
+    <v-app-bar color="primary" prominent>
+      <v-app-bar-nav-icon variant="text" @click.stop="drawerVisible = !drawerVisible"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>{{ pageTitle }}</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn variant="text" icon="mdi-magnify"></v-btn>
+      <v-btn variant="text" icon="mdi-filter"></v-btn>
+      <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawerVisible" location="left" temporary>
+      <v-list :items="items"></v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <RouterView />
+    </v-main>
   </v-app>
 </template>
