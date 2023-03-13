ca8085b66e0fafb2d9e4aabd76fff6d67835db61
Christina Burt <cmburt7@gmail.com>
2023-03-13 19:07:00 -0400

    scaffolded login view

--- ui/src/views/LoginView.vue
+++ ui/src/views/LoginView.vue
@@ -1,10 +1,71 @@
 <script lang="ts">
 
-export default {};
+export default {
+    data: () => ({
+      form: false,
+      userName: null,
+      password: null,
+      loading: false,
+    }),
+
+    methods: {
+      onSubmit () {
+        if (!this.form) return
+
+        this.loading = true
+
+        setTimeout(() => (this.loading = false), 2000)
+      },
+      required (v: string) {
+        return !!v || 'Field is required'
+      },
+    },
+};
 </script>
 
 <template>
-  <div>Homepage, bitches!</div>
+  <v-sheet class="bg-deep-purple pa-12" rounded>
+    <v-responsive>
+      <v-card class="mx-auto px-6 py-8">
+        <v-form
+          v-model="form"
+          @submit.prevent="onSubmit"
+        >
+          <v-text-field
+            v-model="userName"
+            :readonly="loading"
+            :rules="[required]"
+            class="mb-2"
+            clearable
+            label="User Name"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="password"
+            :readonly="loading"
+            :rules="[required]"
+            clearable
+            label="Password"
+            placeholder="Enter your password"
+          ></v-text-field>
+
+          <br>
+
+          <v-btn
+            :disabled="!form"
+            :loading="loading"
+            block
+            color="success"
+            size="large"
+            type="submit"
+            variant="elevated"
+          >
+            Sign In
+          </v-btn>
+        </v-form>
+      </v-card>
+    </v-responsive>
+  </v-sheet>
 </template>
 
 <style lang="css"></style>
