b060aff18ca46a4930f03751ccbab0ee59ef06fe
Allison Payne <allison@apayneinthe.app>
2023-04-28 15:48:01 -0400

    Intitial stash setup

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,35 +3,33 @@ import '@mdi/font/css/materialdesignicons.css';
 import '@vuepic/vue-datepicker/dist/main.css';
 import 'v-calendar/dist/style.css';
 import { fa } from 'vuetify/iconsets/fa';
-import 'vuetify/styles';
+//import 'vuetify/styles';
 
 import { createPinia } from 'pinia';
 import SetupCalendar from 'v-calendar';
 import { createApp } from 'vue';
 import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
+// import * as components from 'vuetify/components';
+// import * as directives from 'vuetify/directives';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 
 import App from './App.vue';
 import router from './router';
-import './styles/main.css';
+import './styles/main.scss';
 
 const app = createApp(App);
 const vuetify = createVuetify({
-  theme: {
-    defaultTheme: 'dark',
-  },
-  components,
-  directives,
-  icons: {
-    defaultSet: 'mdi',
-    aliases,
-    sets: {
-      mdi,
-      fa,
-    },
-  },
+   theme: {
+      defaultTheme: 'dark',
+   },
+   icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+         mdi,
+         fa,
+      },
+   },
 });
 // app.component('Datepicker', Datepicker);
 
